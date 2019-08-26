808c4568a36423861225ccbc7d3cf34914191999
Marianne Pyon <mariannepyon@gmail.com>
2019-08-26 14:22:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@
 
 class CashRegister
 
-  attr_accessor :total, :discount, :items, :last_transaction_amount
+  attr_accessor :total, :discount, :last_transaction_amount, :items
 
   def initialize(discount = 0)
     @total = 0
